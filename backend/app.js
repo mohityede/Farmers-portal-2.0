8e2c189b4b7344a3417f24273aa537cccf8db852
mohityede <mohityede755@gmail.com>
2023-05-05 18:23:07 +0530

    basic setup for errorHandler

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import errors from "./middlewares/errors.js";
+import ErrorHandler from "./utils/ErrorHandler.js";
 
 const app = express();
 
-app.use("/", (req, res) => {
+// config
+dotenv.config({ path: "./.env" });
+
+app.use("/", (req, res, next) => {
+    const user = undefined;
+    if (!user) {
+        return next(new ErrorHandler("User not found", 404));
+    }
     res.status(200).json({
         success: true,
         massage: "This is home page"
     })
 })
+
+// for errorhandling
+app.use(errors);
 
 app.listen(process.env.PORT || 8000, () => {
     console.log("Server is running on port 8000");
---
+++ backend/middlewares/errors.js
@@ -0,0 +1,13 @@
+import ErrorHandler from "../utils/ErrorHandler.js";
+
+const errors = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+
+    res.status(err.statusCode).json({
+        success: false,
+        message: err.message
+    })
+}
+
+export default errors;
